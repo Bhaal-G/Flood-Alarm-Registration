d32ab4cbf3f3acae950a5f926f3e8b17fd3a72fa
Nipurn <nbhaal0805@gmail.com>
2024-03-26 23:40:17 +0530

    v22

--- backend2/api/index.js
+++ backend2/api/index.js
@@ -43,7 +43,6 @@ const User = mongoose.model("civilPrj", userSchema);
 // Nodemailer transporter setup
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  port: 465,
   secure: true,
   auth: {
     user: process.env.EMAIL_ADDRESS,
